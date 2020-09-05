c19019e67315b1eb20f9131003b45859c414c936
tannikaM <tannika.majumder@belvilla.com>
2020-09-05 23:50:27 +0530

    adding method for put

--- src/main/java/com/retail/controllers/RetailController.java
+++ src/main/java/com/retail/controllers/RetailController.java
@@ -4,10 +4,7 @@ import com.retail.Entity.Price;
 import com.retail.Entity.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.xml.ws.Response;
 
@@ -23,6 +20,16 @@ public class RetailController {
 
 
         return product;
+    }
+
+    @PutMapping (value = "/{id}", produces = "application/json")
+    public void UpdateDetail(@PathVariable String id) {
+//        Price currentPrice = new Price(10.00, "INR");
+//        Product product =  new Product(id, "abc", currentPrice);
+//
+//
+//
+//        return product;
     }
 
 }
