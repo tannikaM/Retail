5a7566ffa1e4c877af3875b0c72b8cdac61a0bc1
tannikaM <tannika.majumder@belvilla.com>
2020-09-05 23:53:40 +0530

    removing extra lines

--- src/main/java/com/retail/controllers/RetailController.java
+++ src/main/java/com/retail/controllers/RetailController.java
@@ -17,19 +17,12 @@ public class RetailController {
         Price currentPrice = new Price(10.00, "INR");
         Product product =  new Product(id, "abc", currentPrice);
 
-
-
         return product;
     }
 
     @PutMapping (value = "/{id}", produces = "application/json")
     public void UpdateDetail(@PathVariable String id) {
-//        Price currentPrice = new Price(10.00, "INR");
-//        Product product =  new Product(id, "abc", currentPrice);
-//
-//
-//
-//        return product;
+
     }
 
 }
